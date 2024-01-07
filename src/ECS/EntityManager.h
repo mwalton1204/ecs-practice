278a7792115f9e8f2c98379c4771dd6c3a9e577e
Michael <michael.walton@g.austincc.edu>
2024-01-07 16:42:39 -0600

    wrote class declaration for EntityManager class

--- src/ECS/EntityManager.h
+++ src/ECS/EntityManager.h
@@ -1,6 +1,27 @@
 #ifndef ENTITY_MANAGER_H
 #define ENTITY_MANAGER_H
 
+#include <queue>
+#include <array>
+#include "ECSConstants.h"
 
+class EntityManager {
+    public:
+
+        EntityManager();
+        ~EntityManager();
+
+        Entity createEntity();
+        void destroyEntity(Entity entity);
+        void setFlags(Entity entity, Signature signature);
+        void getFlags(Entity entity);
+
+    private:
+
+    std::queue<Entity> m_availableEntities{};
+    std::array<Signature, MAX_ENTITIES> m_signatures{};
+    uint32_t m_livingEntities;
+
+};
 
 #endif //ENTITY_MANAGER_H
