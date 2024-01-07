0c7ac61ff228b8fb3d3e91a04625f231bf49e073
Michael <michael.walton@g.austincc.edu>
2024-01-07 15:18:02 -0600

    modified normalize function for readability

--- src/Utils/VectorUtils.cpp
+++ src/Utils/VectorUtils.cpp
@@ -1,14 +1,18 @@
 #include "VectorUtils.h"
 
 namespace VectorUtils {
+
     sf::Vector2f normalize(const sf::Vector2f& vector) {
+        sf::Vector2f normalizedVector;
         float magnitude = std::sqrt(vector.x * vector.x + vector.y * vector.y);
 
         if (magnitude != 0) {
-            return sf::Vector2f(vector.x / magnitude, vector.y / magnitude);
+            normalizedVector = {vector.x / magnitude, vector.y / magnitude};
         } else {
-            // Handle the case where the vector has zero magnitude to avoid division by zero.
-            return sf::Vector2f(0.f, 0.f);
+            normalizedVector = {0.f, 0.f};
         }
+
+        return normalizedVector;
     }
+
 }
