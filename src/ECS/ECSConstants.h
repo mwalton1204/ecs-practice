88ddc38077609755513463f2c04aaec4fbfcf7b7
Michael <michael.walton@g.austincc.edu>
2024-01-07 16:16:28 -0600

    defined compnentFlags as a bitset of size MAX_COMPONENTS

--- src/ECS/ECSConstants.h
+++ src/ECS/ECSConstants.h
@@ -2,11 +2,14 @@
 #define ENTITY_H
 
 #include <cstdint>
+#include <bitset>
 
 using Entity = std::uint32_t; // Defines an entity as a 32-bit integer
 const Entity MAX_ENTITIES = 10000;
 
 using Component = std::uint8_t; // Defines a component as an 8-bit integer
 const Component MAX_COMPONENTS = 50;
+
+using componentFlags = std::bitset<MAX_COMPONENTS>; // For tracking which components an entity has
 
 #endif //ENTITY_H
