307a339e199190fad47e8315290479b99951d63d
Michael <michael.walton@g.austincc.edu>
2024-01-07 20:22:18 -0600

    modified max entities value

--- src/ECS/ECSConstants.h
+++ src/ECS/ECSConstants.h
@@ -5,7 +5,7 @@
 #include <bitset>
 
 using Entity = std::uint32_t; // Defines an entity as a 32-bit integer
-const Entity MAX_ENTITIES = 10000;
+const Entity MAX_ENTITIES = 5000;
 
 using Component = std::uint8_t; // Defines a component as an 8-bit integer
 const Component MAX_COMPONENTS = 50;
