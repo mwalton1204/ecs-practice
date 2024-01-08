504758c1d0b59844e0420a612856abef00567ba1
Michael <michael.walton@g.austincc.edu>
2024-01-08 11:07:28 -0600

    ComponentArray class declaration

--- src/ECS/ComponentArray.h
+++ src/ECS/ComponentArray.h
@@ -1,4 +1,6 @@
 #include "IComponentArray.h"
+#include <array>
+#include <unordered_map>
 
 template<typename ComponentType>
 
@@ -6,6 +8,16 @@ class ComponentArray : public IComponentArray {
 
     public:
 
+        void InsertData(Entity entity, ComponentType component;)
+        void RemoveData(Entity entity)
+        ComponentType& GetData(Entity entity)
+        void EntityDestroyed(Entity entity) override
+
     private:
 
+        std::array<ComponentType, MAX_ENTITIES> m_componentArray;
+        std::unordered_map<Entity, size_t> m_EntityToIndexMap;
+        std::unordered_map<size_t, Entity> m_indexToEntityMap;
+
+        size_t m_size;
 };
