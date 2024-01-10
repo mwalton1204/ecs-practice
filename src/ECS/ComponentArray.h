dbd01336411aebed5128bd7c61011a6c06a6569f
Michael <michael.walton@g.austincc.edu>
2024-01-10 11:52:30 -0600

    moved implementation to header to avoid conflicts with template class

--- src/ECS/ComponentArray.h
+++ src/ECS/ComponentArray.h
@@ -1,4 +1,4 @@
-#ifndef I_COMPONENT_ARRAY_H
+#ifndef COMPONENT_ARRAY_H
 #define COMPONENT_ARRAY_H
 
 #include "IComponentArray.h"
@@ -11,10 +11,48 @@ class ComponentArray : public IComponentArray {
 
     public:
 
-        void insertData(Entity, ComponentType);
-        void removeData(Entity);
-        ComponentType& getData(Entity);
-        void entityDestroyed(Entity) override;
+        void insertData(Entity entity, ComponentType component) {
+            assert(m_entityToIndexMap.find(entity) == m_entityToIndexMap.end() && "Component added to same entity more than once.");
+
+            // Put new entry at end and update the maps
+            size_t newIndex = mSize;
+            m_entityToIndexMap[entity] = newIndex;
+            m_indexToEntityMap[newIndex] = entity;
+            m_componentArray[newIndex] = component;
+            mSize++;
+        }
+
+        void removeData(Entity entity) {
+            assert(m_entityToIndexMap.find(entity) != m_entityToIndexMap.end() && "Removing non-existent component.");
+
+            // Copy element at end into deleted element's place to maintain density
+            size_t indexOfRemovedEntity = m_entityToIndexMap[entity];
+            size_t indexOfLastElement = mSize - 1;
+            m_componentArray[indexOfRemovedEntity] = m_componentArray[indexOfLastElement];
+
+            // Update map to point to moved spot
+            Entity entityOfLastElement = m_IndexToEntityMap[indexOfLastElement];
+            m_entityToIndexMap[entityOfLastElement] = indexOfRemovedEntity;
+            m_indexToEntityMap[indexOfRemovedEntity] = entityOfLastElement;
+
+            m_entityToIndexMap.erase(entity);
+            m_indexToEntityMap.erase(indexOfLastElement);
+
+            m_Size--;
+        }
+
+        ComponentType& getData(Entity entity) {
+            assert(m_entityToIndexMap.find(entity) != m_entityToIndexMap.end() && "Retrieving non-existent component.");
+
+            // Return a reference to the entity's component
+            return m_componentArray[m_entityToIndexMap[entity]];
+        }
+
+        void entityDestroyed(Entity entity) override {
+            if (m_entityToIndexMap.find(entity) != m_entityToIndexMap.end()) {
+                RemoveData(entity);
+            }
+        }
 
     private:
 
@@ -24,7 +62,5 @@ class ComponentArray : public IComponentArray {
 
         size_t m_size;
 };
-
-#include "ComponentArray.tpp"
 
 #endif //COMPONENT_ARRAY_H
