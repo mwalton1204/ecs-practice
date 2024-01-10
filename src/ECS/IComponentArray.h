3d087b4b4a3da1bd943be26da430fc1bee98832f
Michael <michael.walton@g.austincc.edu>
2024-01-10 17:14:30 -0600

    corrected casing for entityDestroyed method

--- src/ECS/IComponentArray.h
+++ src/ECS/IComponentArray.h
@@ -7,7 +7,7 @@ class IComponentArray
 {
     public:
         virtual ~IComponentArray() = default;
-        virtual void EntityDestroyed(Entity entity) = 0;
+        virtual void entityDestroyed(Entity entity) = 0;
 };
 
 #endif //I_COMPONENT_ARRAY_H
